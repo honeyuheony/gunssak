681adf81a063e1cc5a029fec5580d4cb1f85c524
Dltmd202 <dltmd202@gmail.com>
2021-08-13 23:53:12 +0900

    Add set_lecture_all_zero set_base_time

--- sugang/lecture/views.py
+++ sugang/lecture/views.py
@@ -3,14 +3,24 @@ from django.db.models import Q
 from lecture.models import *
 import pandas as pd
 
+
 # Create your views here.
 def set_time_table():
-    weeks = ['mon', 'tue', 'wen', 'thu', 'fri']
-    for i, week in enumerate(weeks):
-        for j in range(0, 18):
-            t = Time()
-            t.week = week
-            t.day = j
+    weeks = ['월', '화', '수', '목', '금']
+    day_times = range(1, 9)
+    day_parts = ['A', 'B']
+    pk = 1
+    for week in weeks:
+        for day_time in day_tiems:
+            for day_part in day_parts:
+                t = Time.objects.create()
+                t.week = week
+                t.day = (day_time + day_part)
+                t.slug = slugify(pk)
+                t.save()
+                pk += 1
+    return redirect('/')
+
 
 
 def set_department_table():
@@ -22,10 +32,23 @@ def get_users_timetable(request):
     while user.credit > 18:
         lecture = Lecture.objects.filter(
             Q(max_user__gt=current_user) &
-            Q(department__slug__exact=current_user.department)
+            Q(department__slug__exact=user.department)
         ).order_by("?")
-        if user.credit + lecture.credit < 21:
-            if not lecture in user.lecture:
-                user.lecture.add(lecture)
-                lecture.current_user += 1
+        if lecture.count() > 0:
+            if user.credit + lecture.credit < 21:
+                if not lecture in user.lecture:
+                    user.lecture.add(lecture)
+                    lecture.current_user += 1
+        else:
+            break
     user.save()
+    return redirect('/')
+
+
+def set_lecture_all_zero(request):
+    lectures = Lecture.objects.iterator()
+    for lecture in lectures:
+        lecture.current_user = 0
+        lecture.save()
+    return redirect('/')
+
