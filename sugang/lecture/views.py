f5d82f357aa968c0417c077e9078cfb01991798a
Kuhy <kwh1019_@naver.com>
2021-08-14 02:25:38 +0900

    update html

--- sugang/lecture/views.py
+++ sugang/lecture/views.py
@@ -5,45 +5,8 @@ import pandas as pd
 from django.utils.text import slugify
 
 
-import csv
-
-CSV_PATH = './data.csv'  # 3. csv 파일 경로
-# Create your views here.
-with open(CSV_PATH, newline='') as csvfile:  # 4. newline =''
-    data_reader = csv.DictReader(csvfile)
-    day = []
-    for row in data_reader:
-        time = row['time']
-        time = time.split(",")
-        dayStart = ""
-        dayEnd = ""
-        for t in time:
-            dayStart = t[0:1] + t[1:3]
-            dayEnd = t[0:1] + t[5:7]
-            for i in range(int(dayStart[1:3]), int(dayEnd[1:3] + 1)):
-                day.append(i + "A")
-                day.append(i + "B")
-            if dayStart[3:4] == "B":
-                day.remove(dayStart[1:3] + "A")
-            if dayEnd[3:4] == "A":
-                day.remove(dayEnd[1:3] + "B")
-        lecture = Lecture.objects.create(
-            name=row['name'],
-            code=row['code'],
-            semester=row['semester'],
-            department=row['department'],
-            division=row['division'],
-            credit=row['credit'],
-            time=row['time'],
-            capacity=row['capacity'],
-            grade=row['grade']
-
-            # 6
-        )
-
-        for d in day:
-            time = Time.objects.get(slug=d)
-            lecture.time.add(time)
+def main(request):
+    return render(request, 'main.html')
 
 
 def set_time_table():
