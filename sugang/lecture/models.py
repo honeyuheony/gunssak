99c0aed458d3bf64d248631cf627a8e1728590aa
Dltmd202 <dltmd202@gmail.com>
2021-08-13 21:20:21 +0900

    db modeling:

---
+++ sugang/lecture/models.py
@@ -0,0 +1,42 @@
+from django.db import models
+
+
+# 수강시간
+class Time(models.Model):
+    week = models.CharField(max_length=100)
+    day = models.CharField(max_length=100)
+    slug = models.SlugField(max_length=150, allow_unicode=True, unique=True)
+
+
+
+# 학부
+class Department(models.Model):
+    name = models.CharField(max_length=100)
+    slug = models.SlugField(max_length=150, allow_unicode=True, unique=True)
+
+
+# 분반
+class Divsion(models,Model):
+    name = models.CharField(max_length=100)
+    slug = models.SlugField(max_length=150, allow_unicode=True, unique=True)
+
+
+
+# Create your models here.
+class Lecture(models.Model):
+    name = models.CharField()
+    code = models.TextField()
+    semester = models.PositiveIntegerField()
+    department = models.ManyToManyField(Department, blank=True, null=True)
+    credit = models.PositiveIntegerField(default=0)
+    time = models.ManyToManyField(Time, blank=True, null=True)
+    grade = model.PositiveIntegerField()
+
+
+class CustomUser(models.Model):
+    credit = models.PositiveIntegerField(default=0)
+    name = models.CharField(max_length=100)
+    time = models.ManyToManyField(Time, blank=True, null=True)
+    lecture = models.ManyToManyField(Lecture, blank=True, null=True)
+    department = models.ForeignKey(Department, blank=True, null=True)
+    grade = models.PositiveIntegerField()
---
+++ sugang/lecture/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+from lecture.models import *
+import pandas as pd
+
+# Create your views here.
+def set_time_table():
+    weeks = ['mon', 'tue', 'wen', 'thu', 'fri']
+    for i, week in enumerate(weeks):
+        for j in range(0, 18):
+            t = Time()
+            t.week = week
+            t.day = j
+
+
+def set_department_table():
+    department = ['메카트로닉스공학부', '교양학부', 'HRD학과', ]
+
+
+def read_exel():
+    df = pd.read_exel()
