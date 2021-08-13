b8c30e7ebd9c27692fb0b0ce1bd832ab181e6e42
Dltmd202 <dltmd202@gmail.com>
2021-08-13 23:25:01 +0900

    Add Function of setting user's timetable

--- sugang/lecture/views.py
+++ sugang/lecture/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.db.models import Q
 from lecture.models import *
 import pandas as pd
 
@@ -16,5 +17,15 @@ def set_department_table():
     department = ['메카트로닉스공학부', '교양학부', 'HRD학과', ]
 
 
-def read_exel():
-    df = pd.read_exel()
+def get_users_timetable(request):
+    user = request.user
+    while user.credit > 18:
+        lecture = Lecture.objects.filter(
+            Q(max_user__gt=current_user) &
+            Q(department__slug__exact=current_user.department)
+        ).order_by("?")
+        if user.credit + lecture.credit < 21:
+            if not lecture in user.lecture:
+                user.lecture.add(lecture)
+                lecture.current_user += 1
+    user.save()
